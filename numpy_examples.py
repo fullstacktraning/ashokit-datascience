c4830cad869d97f0c91ae2e4fecf973422838360
Samba <sivasowjanya.p@gmail.com>
2026-02-16 20:58:44 +0530

    numpy started

---
+++ numpy_examples.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+# print(arr[0],arr[-5])
+# print(arr[2],arr[-3])
+# print(arr[0])
+# print(arr[-1])
+# print(arr[1:4])
+
+
+# arr = np.arange(1,25)
+# print(arr.reshape(2,3,4))
+
+
+# arr = np.arange(1,10)
+# print(arr.reshape(3,3))
+
+
+# arr = np.arange(1,10,2)
+# print(arr)
+# print(arr.shape)
+
+
+# arr = np.eye(3)
+# print(arr)
+
+# arr = np.ones((3,3))
+# print(arr)
+
+# arr = np.zeros((3,3))
+# print(arr)
+
+
+
+# import numpy as np
+# arr = np.array([[1,2,3],
+#                 [4,5,6],
+#                 [7,8,9]])
+# print(arr)
+# print(arr.shape)
+
+
+
+
+# import numpy as np
+# arr = np.array([1,2,3,4,5])
+# print(arr)
+# print(type(arr))
