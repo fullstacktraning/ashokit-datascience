2ad4538ee637f3022e5cd98a4cbc5da9a746268a
Samba <samba.pendela@gmail.com>
2026-04-17 21:34:30 +0530

    cnn completed

--- DeepLearning/cnn-pneumnia-appln/download.py
+++ DeepLearning/cnn-pneumnia-appln/download.py
@@ -1,6 +1,7 @@
 import os
+import zipfile
 # download dataset
 os.system("kaggle datasets download -d paultimothymooney/chest-xray-pneumonia")
 # unzip
-os.system("unzip chest-xray-pneumonia.zip")
-
+with zipfile.ZipFile("chest-xray-pneumonia.zip", 'r') as zip_ref:
+    zip_ref.extractall("data")
